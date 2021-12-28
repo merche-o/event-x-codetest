6e887bad401cb489ee58d61704272da621d3ebab
merche-o <olivier.mercher@epitech.eu>
2021-12-28 12:12:18 +0900

    fix npm build issue

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Main from './main'
-import { Provider } from 'react-redux';
-import store from '../redux/store'
+
 
 export default function Home() {
   return (
@@ -12,9 +11,7 @@ export default function Home() {
         <meta name="description" content="EventX demo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Provider store={store}>
       <Main />
-      </Provider>
       <footer className={styles.footer}>
         Made by Olivier Mercher
       </footer>
